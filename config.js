1a68a8a5a3eb0f8cbe0bcf777210ffd507ccf251
Wedding_propose <0c.c0mutt@gmail.com>
2025-08-05 17:40:29 +0900

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ export const KAKAOTALK_API_TOKEN = "JavaScript 키 입력";
 export const KAKAOTALK_SHARE_IMAGE =
   "https://cdn.pixabay.com/photo/2014/11/13/17/04/heart-529607_960_720.jpg";
 
-export const WEDDING_DATE = "1970년 01월 01일, 목요일 오전 12시 00분";
+export const WEDDING_DATE = "2025년 08월 31일, 목요일 오전 12시 00분";
 export const WEDDING_LOCATION = "○○○웨딩, ○층 ○○홀";
 
 export const GROOM_NAME = "○○○";
